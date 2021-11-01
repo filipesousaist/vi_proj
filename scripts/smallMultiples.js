acd2baf0e6f0daf20c23cd71b0fa26e2e9974761
filipesousaist <filipe.miguel.sousa@tecnico.ulisboa.pt>
2021-11-01 01:28:19 +0000

    horizontal small multiples

--- scripts/smallMultiples.js
+++ scripts/smallMultiples.js
@@ -1,8 +1,8 @@
 
-const margin = { top: 20, right: 20, bottom: 80, left: 40 };
+const margin = { top: 20, right: 10, bottom: 20, left: 180 };
 
-const width = 300 - margin.left - margin.right;
-const height = 150 - margin.top - margin.bottom;
+const width = 400 - margin.left - margin.right;
+const height = 140 - margin.top - margin.bottom;
 const titleHeight = 50;
 
 function createSmallMultiples(numAndPeakPlayersPerTag, playerCounts, update) {
@@ -58,7 +58,7 @@ function createSmallMultiples(numAndPeakPlayersPerTag, playerCounts, update) {
             .attr("font-weight", "bolder");
 }
 
-function createBarChart(data, tag, chartNum, update) {
+function createBarChart2(data, tag, chartNum, update) {
     data.splice(Math.min(5, data.length));
 
     for (let row of data)
@@ -158,6 +158,116 @@ function createBarChart(data, tag, chartNum, update) {
                 .attr("y", d => y(d["num"]))
                 .attr("width", x.bandwidth())
                 .attr("height", d => height - y(d["num"]))
+                .attr("fill", g_tagToColor[tag])
+                .select("title")
+                .text(d => d["name"] + ": " + round(d["num"], 2)),
+            (exit) => exit.remove()
+        );	
+}
+
+function createBarChart(data, tag, chartNum, update) {
+    data.splice(Math.min(5, data.length));
+
+    for (let row of data)
+        row["name"] = g_idToName[row["id"]];
+
+
+    const x = d3.scaleLinear()
+        .domain([0, 1.1 * d3.max(d3.map(data, d => d["num"]))])
+        .range([0, width]);
+	
+    const y = d3
+        .scaleBand()
+        .domain(data.map(d => d["name"]))
+        .range([0, height])
+        .padding(0.4);
+		
+    const yAxis = d3
+        .axisLeft()
+        .scale(y)
+        .tickSizeOuter(0);
+		
+	const xAxis = d3
+        .axisBottom()
+        .scale(x)
+        .tickSizeOuter(0)
+        .ticks(5)
+        .tickFormat(d => d < 1000 ? d : (d / 1000) + "K");
+
+    if (!update)
+        d3
+            .select("div#small" + chartNum)
+            .append("svg")
+            .append("g");
+		
+	const svg = d3
+        .select("div#small" + chartNum)
+        .select("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .select("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    if (!update) {
+        svg
+            .append("text")
+            .attr("class", "title");
+        svg
+            .append("g")
+            .attr("class", "xAxis");
+        svg
+            .append("g")
+            .attr("class", "yAxis");
+        svg
+            .append("g")
+            .attr("class", "bars");   
+    }
+
+    svg
+        .select("text.title")
+        .text(tag)
+        .attr("text-anchor", "middle")
+        .attr("font-family", "Arial")
+        .attr("font-weight", "bolder")
+        .attr("fill", g_tagToColor[tag])
+        .attr("x", width / 2);
+
+    svg
+        .select("g.yAxis")
+        .call(yAxis)
+        .selectAll("text")  
+        .style("text-anchor", "end")
+        .attr("font-family", "Arial")
+        .attr("font-weight", "bolder");
+        //.attr("dx", "-.8em")
+        //.attr("dy", ".15em")
+        //.attr("transform", "rotate(-45)");
+	
+    svg
+        .select("g.xAxis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+	
+	svg
+		.select("g.bars")
+		.selectAll("rect")
+		.data(data, d => d["num"])
+        .join(
+            (enter) => enter
+                .append("rect")
+                .attr("x", d => 0)
+                .attr("y", d => y(d["name"]))
+                .attr("width", d => x(d["num"]))
+                .attr("height", y.bandwidth())
+                .attr("fill", g_tagToColor[tag])
+                .append("title")
+                .text(d => d["name"] + ": " + round(d["num"], 2)),
+            (update) => update
+                .select("rect")
+                .attr("x", d => x(d["num"]))
+                .attr("y", d => y(d["name"]))
+                .attr("width", d => width - x(d["num"]))
+                .attr("height", y.bandwidth())
                 .attr("fill", g_tagToColor[tag])
                 .select("title")
                 .text(d => d["name"] + ": " + round(d["num"], 2)),

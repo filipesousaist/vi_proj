92ec05afb0f3d7d8947f21f185256eecee075f6c
filipesousaist <filipe.miguel.sousa@tecnico.ulisboa.pt>
2021-11-01 14:06:16 +0000

    Add tags via text input

--- scripts/tags.js
+++ scripts/tags.js
@@ -1,15 +1,21 @@
 const tagContainer = document.querySelector('.tag-container');
 const input = document.querySelector('.tag-container input');
-input.placeholder = "Search tag here";
+input.placeholder = "Search tag here...";
 
 let tags = [];
 
 function createTag(label) {
   const div = document.createElement('div');
   div.setAttribute('class', 'tag');
+  //div.setAttribute('style', 'background-color: ' + g_tagToColor[label] + ';');
   
 	const span = document.createElement('span');
   span.innerHTML = label;
+  span.setAttribute('style', 
+    'font-family: Arial;' +
+    'font-size: 14;' +
+    'font-weight: bolder;' +
+    'color: ' + g_tagToColor[label] + ';');
   div.appendChild(span);
 	
 	const closeBtn = document.createElement('span');
@@ -35,10 +41,26 @@ function addTags() {
 
 input.addEventListener('keyup', (e) => {
     if (e.key === 'Enter') {
+      let updated = false;
+
       e.target.value.split(',').forEach(tag => {
-        tags.push(tag);  
+        for (let otherTag of tags)
+          if (tag == otherTag)
+            return;
+        let exists = false; 
+        for (let otherTag of g_allTags)
+          if (tag == otherTag) {
+            exists = true;
+            break;
+          }
+        if (exists) {
+          updated = true;
+          g_selectedTags.push(tag);
+          tags.push(tag);
+        }
       });
-      
+      if (updated)
+        updatePlots();
       addTags();
       input.value = '';
     }
@@ -46,7 +68,6 @@ input.addEventListener('keyup', (e) => {
 document.addEventListener('click', (e) => {
   if (e.target.className === "close") {
     const tagLabel = e.target.getAttribute('data-item');
-		console.log(tagLabel)
     const index = tags.indexOf(tagLabel);
 		let i = g_selectedTags.indexOf(tags[index])
 		if (i > -1) {
